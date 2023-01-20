3963dab0e49db0da4be43158590edc14f2e71628
Adeyinka-Meduoye <88234999+Adeyinka-Meduoye@users.noreply.github.com>
2023-01-20 20:33:57 +0100

    Add files via upload

---
+++ question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The open-source ChatGPT as an AI platform is developed by?",
+    answer: "OpenAI",
+    options: [
+      "Microsoft",
+      "Facebook ML Engineers",
+      "OpenAI",
+      "Google Developers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Football team won the 2019 edition of the UEFA Champions League?",
+    answer: "Liverpool FC",
+    options: [
+      "Bayern Munich",
+      "Liverpool FC",
+      "Real Madrid CF",
+      "Chelsea FC"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The 2023 edition of the Spanish Super Cup was played in what country?",
+    answer: "Saudi Arabia",
+    options: [
+      "Spain",
+      "Madrid City",
+      "Qatar",
+      "Saudi Arabia"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The second - t - in HTTPS stands for?",
+    answer: "Transfer",
+    options: [
+      "Trade",
+      "Transfer",
+      "Text",
+      "Technology"
+    ]
+  },
+    
+];
+
